f8192722df128559d798fb94e8c05af5e25354cf
Diák <@petrik.hu>
2026-03-03 08:20:10 +0100

    esemeny osztaly blueprint letrehozasa

---
+++ EsemenykezelesOOP/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsemenykezelesOOP
+{
+	// BLUEPRINT
+	internal class Event
+	{
+		private string name;
+		private int duration;
+
+		public Event(string name, int duration)
+		{
+			this.name = name;
+			this.duration = duration;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Duration { get => duration; set => duration = value; }
+
+
+	}
+}
