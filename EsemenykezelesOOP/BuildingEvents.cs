61483448cb86465a13ab2e6ef4e4ceb3530014a4
Garay Ágoston <garayagoston@gmail.com>
2026-03-06 10:25:47 +0100

    Alap öröklődés

---
+++ EsemenykezelesOOP/BuildingEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsemenykezelesOOP
+{
+	internal class BuildingEvents: Esemenyek
+	{
+		private int durability;
+		private int occupancy;
+
+		protected int Durability { get => durability; set => durability = value; }
+		protected int Occupancy { get => occupancy; set => occupancy = value; }
+
+		public BuildingEvents(string name, int duration, string effect, int durabilty, int occupancy) : base(name, duration, effect)
+		{
+			this.durability = durabilty;
+			this.occupancy = occupancy;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + $"{this.durability} {this.occupancy}";
+		}
+	}
+}
--- EsemenykezelesOOP/MainProgram.cs
+++ EsemenykezelesOOP/MainProgram.cs
@@ -6,23 +6,9 @@ namespace EsemenykezelesOOP
     internal class MainProgram
     {
         static void Main(string[] args)
-        { 
-            EventManager eventManager = new EventManager();
-
-            // Creating events
-            eventManager.StartEvent(new SpecificEvents.PowerOutage());
-            eventManager.StartEvent(new SpecificEvents.EconomicCrisis());
-
-            // Time passing
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"\n-- WEEK {i} --");
-                eventManager.Tick();
-                eventManager.ActiveEvents();
-            }
-
-            Console.WriteLine("Finished... Press any key to exit.");
-            Console.ReadKey();
+        {
+			BuildingEvents b1 = new BuildingEvents("Áramszünet", 10, "ASDFGJ", 100, 50);
+			Console.WriteLine(b1);
         }
 
     }
