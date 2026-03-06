4035380dd3c19b5dda8cd7e78ce5443f13239103
gubamagos <gubamagos@gmail.com>
2026-03-06 17:12:24 +0100

    pre info build

--- EsemenykezelesOOP/NPCEvents.cs
+++ EsemenykezelesOOP/NPCEvents.cs
@@ -8,6 +8,28 @@ namespace EsemenykezelesOOP
 {
 	internal class NPCEvents : Event
 	{
+		private int health;
+		private string religon;
+		private int happiness; // On a scale of 0-100
+		private bool status;
 
-	}
+		// Constructor
+        public NPCEvents(string name, int duration, int health, string religon, int happiness, bool status) : base(name, duration)
+        {
+			this.health = health;
+			this.religon = religon;
+			this.happiness = happiness;
+			this.status = status;
+        }
+
+        public override void ApplyEffect()
+        {
+            Console.WriteLine("Applied effect.");
+        }
+
+        public override void RemoveEffect()
+        {
+            Console.WriteLine("Removed effect.");
+        }
+    }
 }
